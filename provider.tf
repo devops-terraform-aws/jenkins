a442ab1363fd8042e658d350e1f8adef2917df40
developer <ukohae39@gmail.com>
2023-06-16 22:15:49 -0400

    script

--- provider.tf
+++ provider.tf
@@ -1,14 +1,3 @@
 provider "aws" {
   region = var.region
 }
-
-terraform {
-  required_version = ">= 0.14.11"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 4.20.0"
-    }
-  }
-}
